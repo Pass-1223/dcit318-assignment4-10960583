8a3ffce9ad37517983a5f6b715255ba57fa338e8
Pass-1223 <102974001+Pass-1223@users.noreply.github.com>
2024-07-09 14:48:09 +0000

    Add files via upload

---
+++ Canvas/Form1.Designer.cs
@@ -0,0 +1,33 @@
+﻿namespace DrawingApp
+{
+    partial class DrawingForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            // DrawingForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Name = "DrawingForm";
+            this.Text = "Drawing App";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DrawingForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DrawingForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DrawingForm_MouseUp);
+
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Canvas/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingApp
+{
+    public partial class DrawingForm : Form
+    {
+        private bool isDrawing = false;
+        private Point previousPoint;
+
+        public DrawingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawingForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            previousPoint = e.Location;
+        }
+
+        private void DrawingForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, previousPoint, e.Location);
+                }
+                previousPoint = e.Location;
+            }
+        }
+
+        private void DrawingForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+    }
+}
